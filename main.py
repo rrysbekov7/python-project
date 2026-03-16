8e7a4213d6e30ef6c3510b7435bed165834fc0e4
rrysbekov7 <blackfox.97@bk.ru>
2026-03-16 12:47:12 +0600

    16_03_2026 commit#7

---
+++ main.py
@@ -0,0 +1,69 @@
+import openmeteo_requests
+
+
+import os
+import requests
+import pandas as pd
+import requests_cache
+from retry_requests import retry
+
+from dotenv import load_dotenv
+load_dotenv()
+
+def get_weather():
+    # Setup the Open-Meteo API client with cache and retry on error
+    cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+    retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+    openmeteo = openmeteo_requests.Client(session = retry_session)
+
+    # Make sure all required weather variables are listed here
+    # The order of variables in hourly or daily is important to assign them correctly below
+    url = "https://api.open-meteo.com/v1/forecast"
+    params = {
+        "latitude": 52.52,
+        "longitude": 13.41,
+        "hourly": "temperature_2m",
+        "past_days": 1,
+        "forecast_days": 1,
+    }
+    responses = openmeteo.weather_api(url, params=params)
+
+    # Process first location. Add a for-loop for multiple locations or weather models
+    response = responses[0]
+    print(f"Coordinates: {response.Latitude()}°N {response.Longitude()}°E")
+    print(f"Elevation: {response.Elevation()} m asl")
+    print(f"Timezone difference to GMT+0: {response.UtcOffsetSeconds()}s")
+
+    # Process hourly data. The order of variables needs to be the same as requested.
+    hourly = response.Hourly()
+    hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+
+    hourly_data = {"date": pd.date_range(
+        start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
+        end =  pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
+        freq = pd.Timedelta(seconds = hourly.Interval()),
+        inclusive = "left"
+    )}
+
+    hourly_data["temperature_2m"] = hourly_temperature_2m
+
+    hourly_dataframe = pd.DataFrame(data = hourly_data)
+    # print("\nHourly data\n", hourly_dataframe)
+    return hourly_dataframe.to_markdown(index=None)
+    
+def send_message(message):
+    TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_TOKEN")
+    TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHANNEL_ID")
+    
+    url = f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage'
+    params = {
+        'chat_id': TELEGRAM_CHAT_ID,
+        'text': f'''{message}''',
+    }
+    
+    res = requests.get(url, params=params)
+    return res.json()
+
+if __name__ == '__main__':
+    weather = get_weather()
+    send_message(weather)
